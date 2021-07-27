85efe55a609f988efe326e29f0a5e4782c1682b8
naza <tellamaslukaypunto@gmail.com>
2021-07-26 22:05:04 -0300

    nuevo commit creacion de barra de navegación

---
+++ src/components.js
@@ -0,0 +1,9 @@
+const NavBar = () =>
+<nav>
+        <a href = "p">Articulos para gatos</a>
+        <a href = "p">Articulos para perros</a>
+        <a href = "p">Articulos para aves</a>
+        <a href = "p">Articulos para hamsters</a>
+    </nav>
+
+    export default NavBar
---
+++ src/header.js
@@ -0,0 +1,10 @@
+
+import NavBar from "./components"
+
+const Header = () =>
+<header>
+    <h1>Tienda de mascotas</h1>
+    
+</header>
+
+export default Header
